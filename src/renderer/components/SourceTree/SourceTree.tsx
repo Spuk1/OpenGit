cf54f6188f7c1845f941b9db1dc4c207eea68615
leon <leontieber@gmx.net>
2025-06-06 21:02:03 +0200

    implemented Tabs component

--- src/renderer/components/SourceTree/SourceTree.tsx
+++ src/renderer/components/SourceTree/SourceTree.tsx
@@ -18,108 +18,66 @@ const itemRenderer = (treeNode: TreeNode) => (
 
 const gitRepoTree: TreeNode = {
   type: 'directory',
-  uri: '/branches',
+  uri: '/',
   expanded: true,
   children: [
     {
       type: 'directory',
-      uri: '/branches/release',
-      expanded: false,
+      uri: '/branches',
+      expanded: true,
       children: [
         {
-          type: 'file',
-          uri: '/branches/release/v1.0.0',
+          type: 'directory',
+          uri: '/branches/release',
+          expanded: false,
+          children: [
+            { type: 'file', uri: '/branches/release/v1.0.0' },
+            { type: 'file', uri: '/branches/release/v1.1.0' },
+            { type: 'file', uri: '/branches/release/v2.0.0' },
+          ],
         },
         {
-          type: 'file',
-          uri: '/branches/release/v1.1.0',
+          type: 'directory',
+          uri: '/branches/feature',
+          expanded: false,
+          children: [
+            { type: 'file', uri: '/branches/feature/login-refactor' },
+            { type: 'file', uri: '/branches/feature/add-dark-mode' },
+            { type: 'file', uri: '/branches/feature/improve-performance' },
+          ],
         },
-        {
-          type: 'file',
-          uri: '/branches/release/v2.0.0',
-        },
+        { type: 'file', uri: '/branches/main' },
+        { type: 'file', uri: '/branches/develop' },
       ],
     },
     {
       type: 'directory',
-      uri: '/branches/feature',
-      expanded: false,
+      uri: '/remote',
+      expanded: true,
       children: [
         {
-          type: 'file',
-          uri: '/branches/feature/login-refactor',
+          type: 'directory',
+          uri: '/remote/release',
+          expanded: false,
+          children: [
+            { type: 'file', uri: '/remote/release/v1.0.0' },
+            { type: 'file', uri: '/remote/release/v1.1.0' },
+            { type: 'file', uri: '/remote/release/v2.0.0' },
+          ],
         },
         {
-          type: 'file',
-          uri: '/branches/feature/add-dark-mode',
+          type: 'directory',
+          uri: '/remote/feature',
+          expanded: false,
+          children: [
+            { type: 'file', uri: '/remote/feature/login-refactor' },
+            { type: 'file', uri: '/remote/feature/add-dark-mode' },
+            { type: 'file', uri: '/remote/feature/improve-performance' },
+          ],
         },
-        {
-          type: 'file',
-          uri: '/branches/feature/improve-performance',
-        },
+        { type: 'file', uri: '/remote/main' },
+        { type: 'file', uri: '/remote/develop' },
       ],
-    },
-    {
-      type: 'file',
-      uri: '/branches/main',
-    },
-    {
-      type: 'file',
-      uri: '/branches/develop',
-    },
-  ],
-};
-
-const gitRepoRemoteTree: TreeNode = {
-  type: 'directory',
-  uri: '/remote',
-  expanded: true,
-  children: [
-    {
-      type: 'directory',
-      uri: '/remote/release',
-      expanded: false,
-      children: [
-        {
-          type: 'file',
-          uri: '/remote/release/v1.0.0',
-        },
-        {
-          type: 'file',
-          uri: '/remote/release/v1.1.0',
-        },
-        {
-          type: 'file',
-          uri: '/remote/release/v2.0.0',
-        },
-      ],
-    },
-    {
-      type: 'directory',
-      uri: '/remote/feature',
-      expanded: false,
-      children: [
-        {
-          type: 'file',
-          uri: '/remote/feature/login-refactor',
-        },
-        {
-          type: 'file',
-          uri: '/remote/feature/add-dark-mode',
-        },
-        {
-          type: 'file',
-          uri: '/remote/feature/improve-performance',
-        },
-      ],
-    },
-    {
-      type: 'file',
-      uri: '/remote/main',
-    },
-    {
-      type: 'file',
-      uri: '/remote/develop',
     },
   ],
 };
@@ -127,9 +85,6 @@ const gitRepoRemoteTree: TreeNode = {
 export default function SourceTree() {
   const [selected, SetSelected] = useState<Number>(1);
   const [tree, setTree] = useState<TreeNode | undefined>(gitRepoTree);
-  const [remoteTree, setRemoteTree] = useState<TreeNode | undefined>(
-    gitRepoRemoteTree,
-  );
   const [selectedBranch, SetSelectedBranch] =
     useState<string>('/release/v2.0.0');
 
@@ -145,47 +100,35 @@ export default function SourceTree() {
     );
   };
 
-  const toggleExpandedRemote: FileTreeProps['onItemClick'] = (
-    treeNode: TreeNode,
-  ) => {
-    if (treeNode.type !== 'directory') {
-      SetSelectedBranch(treeNode.uri);
-      return;
-    }
-    setRemoteTree((_tree) =>
-      utils.assignTreeNode(_tree, treeNode.uri, {
-        expanded: !treeNode.expanded,
-      }),
-    );
-  };
-
   return (
     <div className="SourceTreeContainer">
-      <div
-        className="Changes"
-        onClick={() => SetSelected(1)}
-        onKeyDown={() => {}}
-        role="button"
-        tabIndex={0}
-        style={{ backgroundColor: selected === 1 ? '#393E46' : '#222831' }}
-      >
-        <span style={{ marginRight: '10px' }}>
-          <GoLog />
-        </span>
-        <span>Changes</span>
-      </div>
-      <div
-        className="Changes"
-        onClick={() => SetSelected(2)}
-        onKeyDown={() => {}}
-        role="button"
-        tabIndex={0}
-        style={{ backgroundColor: selected === 2 ? '#393E46' : '#222831' }}
-      >
-        <span style={{ marginRight: '10px' }}>
-          <GoQuote />
-        </span>
-        <span>All Commits</span>
+      <div className="ChangesContainer">
+        <div
+          className="Changes"
+          onClick={() => SetSelected(1)}
+          onKeyDown={() => {}}
+          role="button"
+          tabIndex={0}
+          style={{ backgroundColor: selected === 1 ? '#393E46' : '#222831' }}
+        >
+          <span style={{ marginRight: '10px' }}>
+            <GoLog />
+          </span>
+          <span>Changes</span>
+        </div>
+        <div
+          className="Changes"
+          onClick={() => SetSelected(2)}
+          onKeyDown={() => {}}
+          role="button"
+          tabIndex={0}
+          style={{ backgroundColor: selected === 2 ? '#393E46' : '#222831' }}
+        >
+          <span style={{ marginRight: '10px' }}>
+            <GoQuote />
+          </span>
+          <span>All Commits</span>
+        </div>
       </div>
       <Divider />
       <FileTree
@@ -196,13 +139,6 @@ export default function SourceTree() {
         draggable // TODO: open context menu on release
       />
       <Divider />
-      {/* <FileTree
-        tree={remoteTree}
-        onItemClick={toggleExpandedRemote}
-        itemRenderer={itemRenderer}
-        activatedUri={selectedBranch}
-        draggable // TODO: open context menu on release
-      /> */}
     </div>
   );
 }
