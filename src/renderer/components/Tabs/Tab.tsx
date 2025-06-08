4291bb0bcbf2fb486407777730f64661fe4d6dc3
leon <leontieber@gmx.net>
2025-06-08 13:23:45 +0200

    tabs now uses actual repositories

--- src/renderer/components/Tabs/Tab.tsx
+++ src/renderer/components/Tabs/Tab.tsx
@@ -3,13 +3,20 @@ import './Tabs.css';
 type Props = {
   name: string;
   active: boolean;
+  onClick: CallableFunction;
 };
 
-export default function Tab({ name, active }: Props) {
+export default function Tab({ name, active, onClick }: Props) {
   return (
     <div
       className="TabContainer"
+      tabIndex={0}
+      role="button"
+      onKeyDown={() => {}}
       style={{ backgroundColor: active ? '#2c323a' : '#222831' }}
+      onClick={() => {
+        onClick();
+      }}
     >
       <div>{name}</div>
     </div>
--- src/renderer/components/Tabs/Tabs.tsx
+++ src/renderer/components/Tabs/Tabs.tsx
@@ -1,10 +1,28 @@
+import { ReactNode, useEffect, useState } from 'react';
+import { useGit } from '../../ContextManager/GitContext';
 import Tab from './Tab';
 
 export default function Tabs() {
-  return (
-    <div className="TabsContainer">
-      <Tab name="OpenGit" active />
-      <Tab name="OpenGit" active={false} />
-    </div>
-  );
+  const { repositories, selectedRepository, setSelectedRepository } = useGit();
+  const [tabs, setTabs] = useState<ReactNode[]>([]);
+
+  useEffect(() => {
+    setTabs(
+      repositories.map((repo) => {
+        const arr = repo.split('/');
+        return (
+          <Tab
+            key={repo}
+            name={arr[arr.length - 1]}
+            active={selectedRepository === arr[arr.length - 1]}
+            onClick={() => {
+              setSelectedRepository(repo);
+            }}
+          />
+        );
+      }),
+    );
+  }, [repositories, selectedRepository, setSelectedRepository]);
+
+  return <div className="TabsContainer">{tabs}</div>;
 }
